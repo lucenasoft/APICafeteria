db1b0b015b8ba0c7f5a4ca0ea34ae76dfedf4715
lucenasoft <getugbr@gmail.com>
2023-02-05 13:04:42 -0300

    feat: config with DB(postgresql)

--- src/main/java/com/api/cafeteria/CafeteriaApplication.java
+++ src/main/java/com/api/cafeteria/CafeteriaApplication.java
@@ -1,4 +1,4 @@
-package com.cafeteria.cafeteria;
+package com.api.cafeteria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
